5d968ca40ed286ca1ac0b5b496e63a7ff441f960
Arnaud Manaranche <arnaud.mana@gmail.com>
2019-11-26 22:38:30 +0100

    feat(studio): display meme selected, add toggle for button actions

--- src/ts/containers/Studio.tsx
+++ src/ts/containers/Studio.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { useState } from 'react'
 import Meme from '@shared/models/Meme'
 
 type StudioProps = {
@@ -6,25 +7,34 @@ type StudioProps = {
 }
 
 function Studio({ memes }: StudioProps): JSX.Element {
+  const [state, setState] = useState('memes');
+  const [memeUrl, setMemeSelected] = useState(null)
+  
+
   return (
     <div className="Studio">
       <div className="Studio__content">
-        <span>Select a template</span>
+        {
+          memeUrl !== null ? <img src={memeUrl} width="100%" /> : <span>Select a template</span>
+        }
       </div>
       <aside className="Studio__aside">
+        {/* TODO: each time we update the state, it call the api, need to store meme urls */}
         <div className="buttons__actions">
-          <button>Icone 1</button>
-          <button>Icone 2</button>
+          <button className={state === 'memes' ? 'active' : null} onClick={(): void => setState('memes')}>Memes</button>
+          <button className={state === 'edit' ? 'active' : null} onClick={(): void => setState('edit')}>Edit</button>
         </div>
-        <div className="gallery__memes">
-          {memes.map(
-            (meme: Meme): React.ReactNode => (
-              <article key={meme.id} data-id={meme.id} className="meme__article">
-                <img src={meme.url} alt={meme.name} />
-              </article>
-            )
-          )}
-        </div>
+        {
+          state === 'memes' ? <div className="gallery__memes">
+            {memes.map(
+              ({ id, url, name }: Meme): React.ReactNode => (
+                <article key={id} data-id={id} className="meme__article">
+                  <img onClick={(): void => setMemeSelected(url)} src={url} alt={name} />
+                </article>
+              )
+            )}
+          </div> : <div>Edit your meme</div>
+        }
       </aside>
     </div>
   )
