8207eb896baf624099e2dfdac66a1b5a0b3526a2
viclafouch <victor.dlf@outlook.fr>
2019-11-29 18:06:28 +0100

    feat(textarea): create Component

---
+++ src/ts/components/TextareaExpended/TextareaExtended.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react'
+import { useRef } from 'react'
+import './textarea-extended.scss'
+
+function TextareaExtended(
+  props: React.DetailedHTMLProps<React.TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement>
+): JSX.Element {
+  const textareaRef = useRef(null)
+
+  const handleKeyDown = (): void => {
+    const textarea: HTMLTextAreaElement = textareaRef.current
+    textarea.style.height = 'inherit'
+    const computed: CSSStyleDeclaration = window.getComputedStyle(textarea)
+
+    const height: number =
+      parseInt(computed.getPropertyValue('border-top-width'), 10) +
+      parseInt(computed.getPropertyValue('padding-top'), 10) +
+      textarea.scrollHeight +
+      parseInt(computed.getPropertyValue('padding-bottom'), 10) +
+      parseInt(computed.getPropertyValue('border-bottom-width'), 10)
+
+    textarea.style.height = height + 'px'
+  }
+
+  return <textarea className="Textarea-exdended" {...props} ref={textareaRef} onKeyDown={handleKeyDown}></textarea>
+}
+
+export default TextareaExtended
