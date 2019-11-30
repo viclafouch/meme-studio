74a599ae4cce2a1a7e089aa46d1b9994e89042db
viclafouch <victor.dlf@outlook.fr>
2019-11-30 13:39:19 +0100

    feat(studio): change FontSize of text

--- src/ts/components/Tabs/Customization/Customization.tsx
+++ src/ts/components/Tabs/Customization/Customization.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react'
 import ReactSVG from 'react-svg'
 import { memo } from 'react'
-import { SketchPicker, ColorResult } from 'react-color'
 import Meme from '@shared/models/Meme'
 import './customization.scss'
 import { CanvasProperties } from '@shared/validators'
 import Accordion from '@components/Accordion/Accordion'
 import TextareaExtended from '@components/TextareaExpended/TextareaExtended'
+import ColorPicker from '@components/ColorPicker/ColorPicker'
+import { ColorResult } from 'react-color'
+import InputRangeSlider from '@components/InputRangeSlider/InputRangeSlider'
 
 type CustomizationProps = {
   memeSelected: Meme | null
@@ -29,6 +31,13 @@ function Customization({ memeSelected, canvasProperties, onCustomize }: Customiz
     onCustomize(textsUpdated)
   }
 
+  const handleChangeFontSize = (textId: string, fontSize: number): void => {
+    const textsUpdated = [...canvasProperties.texts]
+    const textIndex = textsUpdated.findIndex(t => t.id === textId)
+    textsUpdated[textIndex].fontSize = fontSize
+    onCustomize(textsUpdated)
+  }
+
   return (
     <div className="Customization">
       {!memeSelected && (
@@ -41,11 +50,18 @@ function Customization({ memeSelected, canvasProperties, onCustomize }: Customiz
         <div className="customization-not-empty">
           <h2>Edit {memeSelected.name}</h2>
           {canvasProperties.texts.map(
-            ({ value, id, color }, i): React.ReactNode => (
+            ({ value, id, color, fontSize }, i): React.ReactNode => (
               <Accordion title={value.trim() || `Text #${i + 1}`} key={id}>
                 <div className="customization-textbox-section">
                   <TextareaExtended rows={1} defaultValue={value} onChange={(e): void => handleChangeText(id, e)} />
-                  <SketchPicker color={color} onChangeComplete={(color): void => handleChangeColor(id, color)} />
+                  <ColorPicker color={color} setColor={(color: ColorResult): void => handleChangeColor(id, color)} />
+                  <InputRangeSlider
+                    max={50}
+                    min={0}
+                    step={1}
+                    value={fontSize}
+                    onChange={(value: number): void => handleChangeFontSize(id, value)}
+                  />
                 </div>
               </Accordion>
             )
