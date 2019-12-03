6d48420b488b855dcf57af5a361d07dd12edcf27
viclafouch <victor.dlf@outlook.fr>
2019-12-03 12:07:38 +0100

    feat(export): can download your meme

--- src/ts/containers/Export.tsx
+++ src/ts/containers/Export.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react'
-import { useEffect, useState, useCallback } from 'react'
+import { useEffect, useState } from 'react'
 import Modal from '@components/Modal/Modal'
 import { wait } from '@utils/index'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 type ExportProps = {
   onClose: Function
@@ -10,7 +11,7 @@ type ExportProps = {
 
 function Export(props: ExportProps): JSX.Element {
   const [isLoading, setIsLoading] = useState<boolean>(true)
-  const [img, setImg] = useState<string>(null)
+  const [img, setImg] = useState<string>('')
 
   useEffect(() => {
     ;(async (): Promise<void> => {
@@ -21,6 +22,8 @@ function Export(props: ExportProps): JSX.Element {
     })()
   }, [])
 
+  const handleDownload = (): void => {}
+
   const handleClose = (): void => {
     props.onClose()
     setImg(null)
@@ -28,7 +31,21 @@ function Export(props: ExportProps): JSX.Element {
 
   return (
     <Modal onClose={handleClose} isLoading={isLoading}>
-      <img src={img} />
+      <div className="Export">
+        <div className="meme-img">
+          <img src={img} />
+        </div>
+        <div className="meme-actions-share">
+          <a download="meme.png" href={img.replace(/^data:image\/png/, 'data:application/octet-stream')}>
+            <button className="share-button" id="share-local">
+              <FontAwesomeIcon icon={['fas', 'download']} />
+            </button>
+          </a>
+          <button className="share-button social-share-button" id="share-twitter">
+            <FontAwesomeIcon icon={['fab', 'twitter']} />
+          </button>
+        </div>
+      </div>
     </Modal>
   )
 }
