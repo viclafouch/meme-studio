cf59f45b7bef9bb3cc898bdaeca6e7de49e2299f
viclafouch <victor.dlf@outlook.fr>
2019-12-27 13:54:13 +0100

    feat(api): useImgflip for dev

--- src/ts/shared/api/index.ts
+++ src/ts/shared/api/index.ts
@@ -3,6 +3,8 @@ import { fetchApi, parseSearchParams } from '@utils/index'
 
 export const API_URL = 'https://meme-studio.herokuapp.com'
 
+const useImgflip = true
+
 /**
  * getMemes
  * @params object - fetch params
@@ -17,8 +19,24 @@ export interface GetMemesInt {
   }
 }
 
-export const getMemes = (query: object, params: object): Promise<GetMemesInt> =>
-  fetchApi(`/memes?${parseSearchParams(query)}`, params).then((response: any) => ({
-    memes: response.items.map((item: object) => new Meme(item)),
-    cursor: response.cursor
-  }))
+export const getMemes = (query: object, params?: object): Promise<GetMemesInt> => {
+  if (useImgflip) {
+    return fetch('https://api.imgflip.com/get_memes')
+      .then(response => response.json())
+      .then((response: any) => ({
+        memes: response.data.memes.map((item: object) => new Meme(item)),
+        cursor: {
+          after: null,
+          before: null
+        }
+      }))
+  } else {
+    fetchApi(`/memes?${parseSearchParams(query)}`, params).then((response: any) => ({
+      memes: response.items.map((item: object) => new Meme(item)),
+      cursor: response.cursor
+    }))
+  }
+}
+
+export const getMeme = (id: string, params?: object): any =>
+  fetchApi(`/memes/${id}`, params).then((response: any) => console.log(response))
