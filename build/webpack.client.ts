6c81a5b307009db267c6bb9f1cd368ba9c7ae611
viclafouch <victor.dlf@outlook.fr>
2020-03-11 17:19:45 +0100

    build: remove /build from gitignore

---
+++ build/webpack.client.ts
@@ -0,0 +1,71 @@
+import { Configuration } from 'webpack'
+import * as path from 'path'
+import { PORT_CLIENT_DEV, IS_DEV } from '../src/shared/config'
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
+const clientConfig: Configuration = {
+  target: "web",
+  resolve: {
+    alias: {
+      '@client/components': path.resolve(__dirname, '../src/client/ts/components'),
+      '@client/containers': path.resolve(__dirname, '../src/client/ts/containers'),
+      '@client/shared': path.resolve(__dirname, '../src/client/ts/shared'),
+      '@client/store': path.resolve(__dirname, '../src/client/ts/store'),
+      '@client/utils': path.resolve(__dirname, '../src/client/ts/utils'),
+      '@client': path.resolve(__dirname, '../src/client/ts')
+    },
+    extensions: ['.tsx', '.scss']
+  },
+  entry: {
+    main: path.join(__dirname, '..', 'src', 'client', 'ts', 'index.tsx'),
+    styles: path.join(__dirname, '..', 'src', 'client', 'scss', 'styles.scss')
+  },
+  output: {
+    path: path.join(__dirname, '..', 'dist', 'client')
+  },
+  devServer: {
+    port: PORT_CLIENT_DEV,
+    overlay: IS_DEV,
+    open: IS_DEV,
+    openPage: `http://localhost:${PORT_CLIENT_DEV}`,
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: IS_DEV,
+              reloadAll: IS_DEV
+            }
+          },
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              prependData: "@import './src/client/scss/global/mixins';"
+            }
+          }
+        ],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, '..', 'src', 'client', 'html', 'index.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[chunkhash:8].css'
+    }),
+    new CopyPlugin([ { from: path.join(__dirname, '..', 'src', 'client', 'img'), to: 'images' } ]),
+    new CopyPlugin([ { from: 'public', to: 'public' } ])
+  ]
+}
+
+export default clientConfig
