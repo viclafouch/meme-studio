b2f0c0800cde4f2e62df1679d46c64d32abd11ca
viclafouch <victor.dlf@outlook.fr>
2020-04-21 22:13:58 +0200

    fix: loading is a gif on safari

--- src/client/ts/components/Loader/Loader.tsx
+++ src/client/ts/components/Loader/Loader.tsx
@@ -3,16 +3,27 @@ import { ReactSVG } from 'react-svg'
 import { useTranslation } from 'react-i18next'
 import './loader.scss'
 
+const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
+
 function Loader(): JSX.Element {
   const { i18n } = useTranslation()
 
-  return (
-    <div className="loader" aria-busy="true">
-      {i18n.language === 'fr' ? (
+  let loader
+
+  if (!isSafari) {
+    loader =
+      i18n.language === 'fr' ? (
         <ReactSVG src="images/dual-ball-fr.svg" wrapper="span" />
       ) : (
         <ReactSVG src="images/dual-ball-en.svg" wrapper="span" />
-      )}
+      )
+  } else {
+    loader = <img src="images/loading.gif" alt="Loading" />
+  }
+
+  return (
+    <div className="loader" aria-busy="true">
+      {loader}
     </div>
   )
 }
--- src/client/ts/containers/Main.tsx
+++ src/client/ts/containers/Main.tsx
@@ -1,14 +1,13 @@
 import * as React from 'react'
 import { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 import { FatalError } from '@client/components/ErrorBoundary/ErrorBoundary'
 import { useMemes, useEditor } from '@client/ts/shared/hooks'
 import { UseEditorInt } from '../shared/validators'
 import Export from './Export'
 import Router from '../routes'
-import { useLocation } from 'react-router-dom'
 import Loader from '@client/components/Loader/Loader'
 import { wait } from '@shared/utils'
-import { setLocalStorage } from '../utils'
 import { hasRecoverVersion } from '@client/utils/helpers'
 
 function Main(): JSX.Element {
