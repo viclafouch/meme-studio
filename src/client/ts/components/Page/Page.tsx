49cd9a1c6c1ad32e176eef3bb039d89cca169763
viclafouch <victor.dlf@outlook.fr>
2020-04-30 23:33:10 +0200

    fix: meta / css

--- src/client/ts/components/Page/Page.tsx
+++ src/client/ts/components/Page/Page.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { Helmet } from 'react-helmet'
-import { Route, RouteProps, useParams } from 'react-router-dom'
+import { Route, RouteProps, useLocation } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 
 interface PagePropsInt extends RouteProps {
@@ -10,9 +10,12 @@ interface PagePropsInt extends RouteProps {
   }
 }
 
+const SITE_URL: string = window.location.origin
+
 const Page = (props: PagePropsInt): JSX.Element => {
   const { i18n } = useTranslation()
   const { pageMeta, children, ...rest } = props
+  const { pathname } = useLocation()
 
   return (
     <Route {...rest}>
@@ -20,6 +23,8 @@ const Page = (props: PagePropsInt): JSX.Element => {
         <html lang={i18n.language} />
         <title lang={i18n.language}>{pageMeta.title}</title>
         <meta name="description" content={pageMeta.description} />
+        <link rel="canonical" href={`${SITE_URL}${pathname}`} />
+        <base target="_blank" href={SITE_URL} />
       </Helmet>
       {children}
     </Route>
