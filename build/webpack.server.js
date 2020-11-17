c00318747d563802e9beace84a35cb45a573f1ab
viclafouch <victor.dlf@outlook.fr>
2020-11-17 13:12:46 +0100

    fix: remove define plugin

--- build/webpack.server.js
+++ build/webpack.server.js
@@ -1,11 +1,8 @@
 const path = require('path')
-const webpack = require('webpack')
 const nodeExternals = require('webpack-node-externals')
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 const WebpackShellPlugin = require('webpack-shell-plugin')
 const Dotenv = require('dotenv-webpack')
-
-console.log('process.env.PORT', process.env.PORT);
 
 module.exports = (env, argv, IS_DEV = argv.mode !== 'production') => ({
   cache: IS_DEV,
@@ -42,17 +39,6 @@ module.exports = (env, argv, IS_DEV = argv.mode !== 'production') => ({
   },
   plugins: [
     new Dotenv(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-      'process.env.DATABASE_URL': JSON.stringify(process.env.DATABASE_URL),
-      'process.env.TWITTER_CONSUMER_KEY': JSON.stringify(process.env.TWITTER_CONSUMER_KEY),
-      'process.env.TWITTER_CONSUMER_SECRET': JSON.stringify(process.env.TWITTER_CONSUMER_SECRET),
-      'process.env.TWITTER_ACCESS_TOKEN': JSON.stringify(process.env.TWITTER_ACCESS_TOKEN),
-      'process.env.TWITTER_ACCESS_TOKEN_SECRET': JSON.stringify(process.env.TWITTER_ACCESS_TOKEN_SECRET),
-      'process.env.PORT': JSON.stringify(process.env.PORT || 5000),
-      'process.env.PRERENDER_TOKEN': JSON.stringify(process.env.PRERENDER_TOKEN),
-      'process.env.USE_SSL': JSON.stringify(process.env.USE_SSL)
-    }),
     new WebpackShellPlugin({
       onBuildEnd: IS_DEV ? ['npx nodemon ./dist/server/index.js --watch dist/server --delay 2.5'] : []
     })
