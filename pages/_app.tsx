33fc23a6793a2e6c293ac24d0c9d944bb1426fca
viclafouch <victor.dlf@outlook.fr>
2022-01-29 15:17:37 +0100

    chore: init eslint rules

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
+import '../styles/globals.css'
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
 }
 
