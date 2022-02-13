f1cef2b5d006c9a10f3d63201ebac44406b563ca
viclafouch <victor.dlf@outlook.fr>
2022-02-13 12:17:47 +0100

    chore: add Meme type

---
+++ src/@types/Meme.d.ts
@@ -0,0 +1,37 @@
+interface MemeBoxText {
+  isUppercase: boolean
+  id: string
+  value: string
+  width: number
+  height: number
+  centerX: number
+  centerY: number
+  rotate: number
+  fontSize: number
+  fontFamily: string
+  boxShadow: number
+  color: string
+  alignVertical: string
+  textAlign: string
+  createdAt: string
+  updatedAt: string
+}
+
+type Lang = 'en' | 'fr'
+
+interface Meme {
+  width: number
+  height: number
+  boxCount: number
+  filename: string
+  id: string
+  translations: {
+    [key in Lang]: {
+      name: string
+      keywords: string
+    }
+  }
+  texts: MemeBoxText[]
+  createdAt: string
+  updatedAt: string
+}
